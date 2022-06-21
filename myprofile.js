3ba5c75b0a031f6635d8211ed2e6e7357038135e
EdikanWizz <you@example.com>
2022-06-21 17:25:33 +0400

    mycommit

---
+++ myprofile.js
@@ -0,0 +1,20 @@
+const myname = "CHUKWUMA WISDOM CHINECHEREM";
+
+
+let height = "5.8ft/1.77m"
+
+let country = "NIGERIA"
+
+
+document.getElementById("myname").innerHTML= 'MY NAME :' + ' ' + myname
+document.getElementById("height").innerHTML='MY HEIGHT:' + ' ' + height
+document.getElementById("country").innerHTML= 'MY COUNTRY:' + ' ' + country
+
+
+var first = console.log("FIRST NUMBER")
+var second = console.log("SECOND NUMBER:")
+var signs = console.log("YOUR MATH SIGN")
+switch(signs){
+    case "1":
+        console.log(first + '+' + second + '=' +(first + second) )
+}
